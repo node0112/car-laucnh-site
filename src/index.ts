8adb2bc70cd989f902105d07bcb76feab8bf82fe
Sid <sid@Sids-MacBook-Pro.local>
2023-07-03 15:16:29 +0530

    created basic layout and addeda a section

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ import {
     AnisotropyPlugin,
     GammaCorrectionPlugin,
 
-    addBasePlugins,
+    //addBasePlugins,
     ITexture, TweakpaneUiPlugin, AssetManagerBasicPopupPlugin, CanvasSnipperPlugin,
 
     IViewerPlugin,
@@ -26,11 +26,18 @@ import {
 } from "webgi";
 import "./styles.css";
 
+import gsap from 'gsap'
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+
+gsap.registerPlugin(ScrollTrigger)
 async function setupViewer(){
 
     // Initialize the viewer
     const viewer = new ViewerApp({
         canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
+        useRgbm: false,
     })
 
     // Add some plugins
@@ -40,23 +47,23 @@ async function setupViewer(){
     await viewer.addPlugin(AssetManagerBasicPopupPlugin)
 
     // Add plugins individually.
-    // await viewer.addPlugin(GBufferPlugin)
+    await viewer.addPlugin(GBufferPlugin)
     // await viewer.addPlugin(new ProgressivePlugin(32))
-    // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
+    await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
     // await viewer.addPlugin(GammaCorrectionPlugin)
-    // await viewer.addPlugin(SSRPlugin)
-    // await viewer.addPlugin(SSAOPlugin)
+    await viewer.addPlugin(SSRPlugin)
+    await viewer.addPlugin(SSAOPlugin)
     // await viewer.addPlugin(DiamondPlugin)
     // await viewer.addPlugin(FrameFadePlugin)
     // await viewer.addPlugin(GLTFAnimationPlugin)
     // await viewer.addPlugin(GroundPlugin)
-    // await viewer.addPlugin(BloomPlugin)
+    await viewer.addPlugin(BloomPlugin)
     // await viewer.addPlugin(TemporalAAPlugin)
     // await viewer.addPlugin(AnisotropyPlugin)
     // and many more...
 
     // or use this to add all main ones at once.
-    await addBasePlugins(viewer)
+    //await addBasePlugins(viewer)
 
     // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
     await viewer.addPlugin(CanvasSnipperPlugin)
