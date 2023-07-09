da01aad3f747e7218e6bd8288887e7f790d3a413
Sid <sid@Sids-MacBook-Pro.local>
2023-07-09 23:57:01 +0530

    completed most of the site

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,6 @@ import {
 
     IViewerPlugin,
 
-    // Color, // Import THREE.js internals
-    // Texture, // Import THREE.js internals
 } from "webgi";
 import "./styles.css";
 
@@ -59,70 +57,174 @@ async function setupViewer(){
     // Import and add a GLB file.
     await viewer.load("./assets/car_model.glb")
 
-    // Load an environment map if not set in the glb file
-    // await viewer.setEnvironmentMap((await manager.importer!.importSinglePath<ITexture>("./assets/environment.hdr"))!);
+    viewer.scene.activeCamera.setCameraOptions({controlsEnabled: false})
+    window.scrollTo({top: 0, left: 0 })
 
-    // Add some UI for tweak and testing.
-    const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
-    // Add plugins to the UI to see their settings.
-    uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
-
-    //viewer.scene.activeCamera.position.set(-5,0, 1.5);
-
-
-    function setupScrollAnimation(){
-        const tl = gsap.timeline()
-    
-        // second section
-        tl
-        .to(position, {x: -0.44, y: 7.8,z: 1.9,
-            scrollTrigger:{
-                trigger: '#second', 
-                start: 'top bottom',  
-                end: 'top top', 
-                scrub: true},
-             onUpdate})
-            .to(target, {x: 0.2, y:-0.07,z: 1.61,
-                scrollTrigger:{
-                    trigger: '#second', 
-                    markers: true,
-                    start: 'top bottom',  
-                    end: 'top top', 
-                    scrub: true
-                }})
-
-        // third section
-        .to(position, {x: -0.43, y: -0.26,z: 3.9, 
-            scrollTrigger:{
-                trigger: '#third', 
-                markers: true,
-                start: 'top bottom',  
-                end: 'top top', 
-                scrub: true},
-             onUpdate})
-            .to(target, {x: -0.74, y: 0.05,z: 1.12,
-                scrollTrigger:{
-                    trigger: '#third', 
-                    start: 'top bottom',  
-                    end: 'top top', 
-                    scrub: true},
-                })
-    }
-
-    setupScrollAnimation()
-
-    let needsupdate = true
+    let needsUpdate = true
     function onUpdate(){
-        needsupdate = true
-        viewer.renderer.resetShadows()
+        needsUpdate = true
+        viewer.setDirty()
     }
 
-    viewer.addEventListener('preFrame', ()=>{
-        if(needsupdate){
+    viewer. addEventListener ("preFrame", () => {
+        if(needsUpdate){
             camera.positionUpdated(true)
-            camera.targetUpdated(true)
-            needsupdate = false 
+            needsUpdate = false 
         }
+    })
+
+    scrollAnimation(position, target, onUpdate)
+
+    //setting up the inters obs to prevent ovberscroll of content-section at enxd
+
+    const options = {
+        root: document.querySelector('section-container'),
+        threshold: 0.72,
+        rootMargin: '150px'
+    }
+    const obsElem = document.getElementById('fourth')
+    const footer=  document.querySelector('.footer')
+
+    const observer = new IntersectionObserver(obj =>{
+        console.log(obj[0].intersectionRatio)
+        if(obj[0].isIntersecting){
+         document.querySelector('.section-container').style.overflowY = 'hidden'
+         document.querySelector('.section-container').style.scrollSnapType = 'none'
+        }
+    }, options)
+    
+    const footerObs = new IntersectionObserver(obj =>{
+        console.log(obj[0].intersectionRatio + 'footer')
+        if(!obj[0].isIntersecting){
+         console.log('not interesct')
+         document.querySelector('.section-container').style.overflowY = 'scroll'
+         document.querySelector('.section-container').style.scrollSnapType = 'y mandatory'
+        }
+    })
+
+    observer.observe(obsElem)
+    footerObs.observe(footer)
+
+}
+
+function scrollAnimation(position, target, onUpdate){
+    const tl = gsap.timeline()
+
+    tl.to(position,{
+        x: -0.44, 
+        y: 7.8,
+        z: 1.9,
+        scrollTrigger: {
+            scroller: '.section-container',
+            trigger: '#second',
+            start: 'top bottom',
+            end: 'top top',
+            scrub: true,
+            immediateRender: false
+        },
+        onUpdate
+    })
+    .to(target,{
+        x: 0.2, 
+        y:-0.07,
+        z: 1.61,
+        scrollTrigger: {
+            scroller: '.section-container',
+            trigger: '#second',
+            start: 'top bottom',
+            end: 'top top',
+            scrub: true,
+            immediateRender: false
+        },
+    })
+    .to('#second',{
+        opacity: '1',
+        scrollTrigger: {
+            scroller: '.section-container',
+            trigger: '#second',
+            start: 'top bottom',
+            end: 'top 80%',
+            scrub: true,
+            immediateRender: false
+        },
+    })
+    //third section
+    .to(position,{
+        x: -0.43, 
+        y: -0.26,
+        z: 3.9,
+        scrollTrigger: {
+            scroller: '.section-container',
+            trigger: '#third',
+            start: 'top bottom',
+            end: 'top top',
+            scrub: true,
+            immediateRender: false
+        },
+        onUpdate
+    })
+    .to(target,{
+        x: -0.74, 
+        y: 0.05,
+        z: 1.12,
+        scrollTrigger: {
+            scroller: '.section-container',
+            trigger: '#third',
+            start: 'top bottom',
+            end: 'top top',
+            scrub: true,
+            immediateRender: false
+        },
+    })
+    .to('#third',{
+        opacity: '1',
+        scrollTrigger: {
+            scroller: '.section-container',
+            trigger: '#third',
+            start: 'top bottom',
+            end: 'top 80%',
+            scrub: true,
+            immediateRender: false
+        },
+    })
+    //fourth
+    .to(position,{
+        x: 1.97, 
+        y: -0.11,
+        z: -3.84,
+        scrollTrigger: {
+            scroller: '.section-container',
+            trigger: '#fourth',
+            start: 'top bottom',
+            end: 'top top',
+            scrub: true,
+            immediateRender: false
+        },
+        onUpdate
+    })
+    .to(target,{
+        x: -0.16, 
+        y: 0.13,
+        z: 1.25,
+        scrollTrigger: {
+            scroller: '.section-container',
+            trigger: '#fourth',
+            start: 'top bottom',
+            end: 'top top',
+            scrub: true,
+            immediateRender: false
+        },
+    })
+    .to('#fourth',{
+        opacity: '1',
+        scrollTrigger: {
+            scroller: '.section-container',
+            trigger: '#fourth',
+            start: 'top bottom',
+            end: 'top 80%',
+            scrub: true,
+            immediateRender: false
+        },
     })
 }
 
